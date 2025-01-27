ec3e483d58cc654fdbc59c981f91235f09ff07e8
Bill King <billaking@gmail.com>
2025-01-27 01:15:55 -0800

    You can now choose the file extension
    By default a *.html.md file is created so it opens in obsidian. Toggle will save the file as html.

--- main.ts
+++ main.ts
@@ -5,16 +5,17 @@ interface MarkdownToHtmlPluginSettings {
     imageUrl: string;
     documentUrl: string;
     useAppUrl: boolean;
+    useHtmlExtension: boolean;
 }
 
 const DEFAULT_SETTINGS: MarkdownToHtmlPluginSettings = {
     imageUrl: '/sites/default/files/inline-images/',
     documentUrl: 'https://www.cooljcxii.org/',
     useAppUrl: false,
+    useHtmlExtension: false,
 };
 
 export default class MarkdownToHtmlPlugin extends Plugin {
-
     settings: MarkdownToHtmlPluginSettings;
 
     async onload() {
@@ -31,13 +32,7 @@ export default class MarkdownToHtmlPlugin extends Plugin {
     }
 
     async convertMarkdownToHtml() {
-
-        // check if the user wants to use the app url
-
-        const {imageUrl, documentUrl, useAppUrl} = this.settings;
-
-
-
+        const {imageUrl, documentUrl, useAppUrl, useHtmlExtension} = this.settings;
         const activeFile = this.app.workspace.getActiveFile();
         if (!activeFile) {
             new Notice('No active file found');
@@ -46,38 +41,24 @@ export default class MarkdownToHtmlPlugin extends Plugin {
 
         try {
             let markdownContent = await this.app.vault.read(activeFile);
-
-            // Remove frontmatter if present
             markdownContent = markdownContent.replace(/^---[\s\S]*?---\n/, '');
-
-            // Convert this string '![[image]]' to <img src="image" alt="image">
             markdownContent = markdownContent.replace(/!\[\[([^\]]+)\]\]/g, (match, p1) => {
                 return `<img src="${imageUrl}${p1.replace(/ /g, '-')}" alt="${p1}">`;
             });
-
-            // Convert [[link]] to [link](app://markdown.md/link)
-            // Convert [[link]] to <a href="link">link</a>
             markdownContent = markdownContent.replace(/\[\[([^\]]+)\]\]/g, (match, p1) => {
                 return useAppUrl ? `[${p1}](${p1.replace(/ /g, '-')})` : `<a href="${documentUrl}${p1.replace(/ /g, '-')}">${p1}</a>`;
             });
 
-            console.log('markdownContent', markdownContent);
-
-            // Custom renderer to prevent converting markdown links to HTML links
             const renderer = new Renderer();
             renderer.link = ({href, title, text}) => {
-                console.log('link', {href, title, text});
-                // replace spaces with %20
                 href = href.replace(/ /g, '%20');
                 return `[${text}](${href})`;
             };
 
             marked.use({ renderer });
-
-
             const htmlContent = marked(markdownContent);
             const html = `<div>${htmlContent}</div>`;
-            const htmlFileName = activeFile.basename + '.html.md';
+            const htmlFileName = activeFile.basename + (useHtmlExtension ? '.html' :'.html.md');
 
             const existingFile = this.app.vault.getAbstractFileByPath(htmlFileName);
             if (existingFile) {
@@ -101,7 +82,6 @@ export default class MarkdownToHtmlPlugin extends Plugin {
 
     async saveSettings() {
         await this.saveData(this.settings);
-        console.log('Saved settings:', this.settings);
     }
 
     onunload() {
@@ -124,23 +104,21 @@ class MarkdownToHtmlSettingTab extends PluginSettingTab {
         containerEl.createEl('h2', {text: 'Markdown to Blog post settings'});
         containerEl.createEl('p', {text: 'This plugin will convert most markdown tags into a html structure so that you can paste your note content into a Content Management System (CMS) like Drupal or Wordpress; without having to do it manually.'});
 
-        // Add a radio button to enable or disable the use of app url
         new Setting(containerEl)
-            .setName('Format links as markdown or HTML')
-            .setDesc('Enabled example [Welcome home](Welcome-home) or Disabled example <a href="https://www.cooljcxii.org/Welcome-home">Welcome home</a>')
+            .setName('Display links as markdown')
+            .setDesc('When toggled, links are formatted as [link title](note link). Default are <a href></a> html tags')
             .addToggle((toggle) =>
                 toggle
                     .setValue(this.plugin.settings.useAppUrl)
                     .onChange(async (value) => {
-                            this.plugin.settings.useAppUrl = value;
-                            await this.plugin.saveSettings();
-                        }
-                    )
+                        this.plugin.settings.useAppUrl = value;
+                        await this.plugin.saveSettings();
+                    })
             );
 
         new Setting(containerEl)
-            .setName('Web path to your images folder')
-            .setDesc('URL to prefix the markdown links to. Example for Drupal: /sites/default/files/inline-images/')
+            .setName('Image folder location')
+            .setDesc('Files are stored on the web server. Example for Drupal: /sites/default/files/inline-images/. Disabled if <em>Display links as markdown</em> is enabled')
             .addText(text => text
                 .setPlaceholder('/sites/default/files/inline-images/')
                 .setValue(this.plugin.settings.imageUrl)
@@ -150,8 +128,8 @@ class MarkdownToHtmlSettingTab extends PluginSettingTab {
                 }));
 
         new Setting(containerEl)
-            .setName('Web path to your web documents folder')
-            .setDesc('URL to prefix the markdown links to. Example for Drupal: /node/node_id')
+            .setName('Documents folder location')
+            .setDesc('Documents are stored on the web server. Example for Drupal the path is /node/node_id or /node/alias. Disabled if <em>Display links as markdown</em> is enabled')
             .addText(text => text
                 .setPlaceholder('/')
                 .setValue(this.plugin.settings.documentUrl)
@@ -159,6 +137,18 @@ class MarkdownToHtmlSettingTab extends PluginSettingTab {
                     this.plugin.settings.documentUrl = value;
                     await this.plugin.saveSettings();
                 }));
+
+        new Setting(containerEl)
+            .setName('Export file as html')
+            .setDesc('When toggled, files are saved in the html format.')
+            .addToggle((toggle) =>
+                toggle
+                    .setValue(this.plugin.settings.useHtmlExtension)
+                    .onChange(async (value) => {
+                        this.plugin.settings.useHtmlExtension = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
 
         containerEl.createEl('p', {text: 'To report bug contact me at GitHub https://github.com/billaking'});
     }
